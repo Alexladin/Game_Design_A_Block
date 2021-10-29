00071011eb92f8da8eb1b598b74ada6c37692973
Alex Ladin <ladina24@greenhill.org>
2021-10-29 09:50:01 -0500

    changes

--- window2.py
+++ window2.py
@@ -1,37 +1,39 @@
 #Alex Ladin
+#10/27/21
 
 import pygame as py, os, random, time
-
-os.system ('cls')
-
 py.init()
-black = (0, 0, 0)
-white = (255, 255, 255)
-messages=["ScreenSize", "Background Color", "Object Color", "Sounds on/off"]
-width = 800
-height = 800
-
-window = py.display.set_mode((width, height))
-py.display.set_caption("settings")
-
-
-title_font = py.font.SysFont("arial", 80)
-subtitle_font = py.font.SysFont("arial", 50, italic = True)
+BLACK=(255,255,255)
+WHITE=(0,0,0)
+BLUE=(0,255,0)
+WIDTH = 800
+HEIGHT = 800
+messages=['Instructions','Level 1','Level 2','Settings','Score Board', 'Exit']
+messages2=['Screen size', 'Object Color', 'Sound on/off', 'Back']
+Ssmessages=['Bigger', 'Smaller']
+win=py.display.set_mode((WIDTH,HEIGHT))
+py.display.set_caption("Setting Window")
+#TITLE_FONT= py.font.SysFont(name,size,bold=false, italic= false)
+TITLE_FONT= py.font.SysFont('times new roman', 50, italic=True)
+SUBTITLE_FONT= py.font.SysFont('times new roman', 30, italic=True)
+xbox=25
 wbox=25
-hbox=25
-square = py.Rect(10,10, wbox,hbox)
-def display_title(message):
+square=py.Rect(10,10, wbox, xbox)
+
+def display_message(message):
     py.time.delay(100)
-    text = title_font.render(message, 1, white)
-    window.blit(text, (width/2 - text.get_width()/2, 70))
-    py.display.flip()
+    text=TITLE_FONT.render(message,30, BLACK )
+    win.blit(text, (WIDTH/2-text.get_width()/2, 30) )
+    py.display.update()
+    py.time.delay(10)
+
+def displaySub(subTitle,ysub):
     py.time.delay(100)
-# def display_subtitle(message, x, y):
-#     py.time.delay(100)
-#     text = subtitle_font.render(message, 1, white)
-#     window.blit(text, (x, y))
-#     py.display.flip()
-#     py.time.delay(100)
+    text=SUBTITLE_FONT.render(subTitle,1, BLACK )
+    win.blit(text, (10, ysub ))
+    py.display.update()
+    py.time.delay(10)
+
 
 def display_Menu():
     x=70
@@ -40,33 +42,64 @@ def display_Menu():
     square.y=y
     for i in range(0, len(messages)):
         word= messages[i]
-        py.draw.rect(window,white, square )
-        text=subtitle_font.render(word, 1, white)
-        window.blit(text, (x+wbox+10, y))
+        py.draw.rect(win, BLUE, square)
+        text=SUBTITLE_FONT.render(word,10, BLACK)
+        win.blit(text, (x+wbox+10, y))
         py.display.flip()
         py.time.delay(100)
-        y +=100
+        y += 100
         square.y=y
-run = True
+        py.display.set_caption("Menu Window")
+
+win.fill(WHITE)
+display_message('Menu')
+ysub=200
+py.time.delay(200)
+display_Menu()
+run=True
 while run:
     for eve in py.event.get():
         if eve.type == py.QUIT:
-            run = False
-            py.quit()
-    display_title("settings")
-    py.time.delay(300)
-    
+            run=False
+    if eve.type==py.MOUSEBUTTONDOWN:
+        mouse_pressed=py.mouse.get_pressed()
+        if mouse_pressed[0]:
+            mouse_pos=py.mouse.get_pos()
+            if mouse_pos[0]>=80 and mouse_pos[0]<=500:
+                win.fill(WHITE)
+                display_message("Settings")
+                py.display.set_caption("Setting Window")
+                mouse_pos=py.mouse.get_pos()
+                x=70
+                y=190
+                square.x=x
+                square.y=y
+                for i in range(0, len(messages2)):
+                    word= messages2[i]
+                    py.draw.rect(win, BLUE, square)
+                    text=SUBTITLE_FONT.render(word,10, BLACK)
+                    win.blit(text, (x+wbox+10, y))
+                    py.display.flip()
+                    py.time.delay(100)
+                    y += 100
+                    square.y=y
+            
+            if mouse_pos[1]>80 and mouse_pos[1]<=200:
+                win.fill(WHITE)
+                display_message('Screen Size')
+                py.display.set_caption("Screen Size Window")
+                x=70
+                y=190
+                square.x=x
+                square.y=y
+                for i in range(0, len(Ssmessages)):
+                    word= Ssmessages[i]
+                    py.draw.rect(win, BLUE, square)
+                    text=SUBTITLE_FONT.render(word,10, BLACK)
+                    win.blit(text, (x+wbox+10, y))
+                    py.display.flip()
+                    py.time.delay(100)
+                    y += 100
+                    square.y=y
 
-    # display_subtitle("window size",80, 150)
-    # py.time.delay(300)
-    # display_subtitle("background color", 80, 250)
-    # py.time.delay(300)
-    # display_subtitle("object colors", 80, 350)
-    # py.time.delay(300)
-    # display_subtitle("sound (on/off)", 80, 450)
-    # py.time.delay(300)
-
-
-    display_Menu()
-    print(py.mouse.get_pos())
 py.quit()
